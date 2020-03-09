d64a15fb661d2f019aa7a7f8daf6fc73e04c2f1f
pan_maciek <koziejka.com@gmail.com>
2020-03-09 02:06:46 +0100

    added block operations

--- lab1/lib.c
+++ lab1/lib.c
@@ -13,7 +13,7 @@ typedef struct node {
 
 static block* create_block(int size) {
   block* b = calloc(sizeof(block), 1);
-  b->count = b->size = size;
+  b->size = size;
   b->operations = calloc(sizeof(char*), size);
   return b;
 }
@@ -86,4 +86,47 @@ table* create_table(int size) {
   t->values = calloc(sizeof(block*), size);
   t->size = size;
   return t;
+}
+
+void remove_block(table* table, int block_index) {
+  if (table->values[block_index] == NULL) return;
+  free(table->values[block_index]);
+  table->values[block_index] = NULL;
+}
+
+void remove_operation(table* table, int block_index, int operation_index) {
+  block* block = table->values[block_index];
+  if (block == NULL) return;
+  if (block->operations[operation_index] == NULL) return;
+  free(block->operations[operation_index]);
+  block->operations[operation_index] = 0;
+}
+
+void compare_file_sequence(table* table, char** sequence) {
+  static char* files[2];
+
+  int size = table->size;
+  for (int i = 0; i < size; i++) {
+    if (table->values[i]) free(table->values[i]);
+    files[0] = strtok(sequence[i], ":");
+    files[1] = strtok(NULL, "");
+    table->values[i] = compare_files(files[0], files[1]);
+  }
+}
+
+int operation_count(table* table, int block_index) {
+  if (table == NULL || table->values == NULL) return -1;
+  block* block = table->values[block_index];
+  int count = 0;
+  for (int i = 0; i < block->size; i++)
+    if (block->operations[i]) count++;
+  return count;
+}
+
+int blocks_count(table* table) {
+  if (table == NULL || table->values == NULL) return -1;
+  int count = 0;
+  for (int i = 0; i < table->size; i++) 
+    if (table->values[i]) count++;
+  return count;
 }
--- lab1/lib.h
+++ lab1/lib.h
@@ -18,17 +18,23 @@
 }
 
 typedef struct block {
-  unsigned int size, count;
+  unsigned int size;
   char* files[2];
   char** operations;
 } block;
 
 typedef struct table {
-  unsigned int size, count;
+  unsigned int size;
   block** values;
 } table;
 
 table* create_table(int size);
 block* compare_files(char* file1, char* file2);
+void compare_file_sequence(table* table, char** file_sequence);
+void remove_block(table* table, int block_index);
+void remove_operation(table* table, int block_index, int operation_index);
+
+int operation_count(table* table, int block_index);
+int blocks_count(table* table);
 
 #endif
