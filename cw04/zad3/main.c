57555ce554193805d6d7c51d6c5134d1650bb8c3
pan-maciek <kozieja.maciej@gmail.com>
2020-03-29 15:45:39 +0000

    fixed white space
    (need to modify .vimrc)

--- cw04/zad3/main.c
+++ cw04/zad3/main.c
@@ -7,25 +7,25 @@
 #define on(signal, block)\
 void on_##signal(int sig, siginfo_t* info, void* __) { block; _exit(0); }
 #define no(signal) {\
-	struct sigaction act = {.sa_flags=SA_SIGINFO,.sa_sigaction=on_##signal};\
-	sigaction(signal, &act, NULL); }
+  struct sigaction act = {.sa_flags=SA_SIGINFO,.sa_sigaction=on_##signal};\
+  sigaction(signal, &act, NULL); }
 #define test if (fork() == 0) { 
 #define end ;exit(0); } else wait(NULL);
 
 on(SIGSEGV, printf("si_addr (%p)\n", info->si_addr));
-on(SIGINT, printf("si_uid (%u)\n", info->si_uid));
+on(SIGINT, printf("si_uid  (%u)\n", info->si_uid));
 on(SIGFPE, printf("si_code (%u)\n", info->si_code));
 
 int main() {
-	no(SIGSEGV);
-	no(SIGINT);
-	no(SIGFPE);
+  no(SIGSEGV);
+  no(SIGINT);
+  no(SIGFPE);
 
-	int zero = 0;
-	test int x = 1 / zero end
-	test {
-		int* memory_violation = (int*) 12345; 
-		*memory_violation = 213;
-	} end
-	test raise(SIGINT) end
+  int zero = 0;
+  test int x = 1 / zero end
+  test {
+    int* memory_violation = (int*) 12345; 
+    *memory_violation = 213;
+  } end
+  test raise(SIGINT) end
 }
