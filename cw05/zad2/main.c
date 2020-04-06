e43a5a7e2fadddf0ccbcd6ff40fcc2361c2c6d11
pan-maciek <kozieja.maciej@gmail.com>
2020-04-06 18:37:50 +0200

    using popen to read in main

--- cw05/zad2/main.c
+++ cw05/zad2/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 100
+char buffer[BUFFER_SIZE];
+
+static char* __tmp;
+#define sprintfn(additional_size, pattern, args...) \
+  (sprintf(__tmp = calloc(strlen(pattern) + additional_size, sizeof(char)), pattern, args), __tmp)
 
 int main(int argc, char** argv) {
   if (argc != 2) return 1;
-  dup2(fileno(popen("sort", "w")), STDOUT_FILENO);
-  execlp("cat", "cat", argv[1], NULL);
+  int read;
+  char* program = sprintfn(strlen(argv[1]), "sort %s", argv[1]);
+  FILE* file = popen(program, "r");
+  while((read = fread(buffer, sizeof(char), BUFFER_SIZE, file)))
+    write(STDOUT_FILENO, buffer, read);
 }
